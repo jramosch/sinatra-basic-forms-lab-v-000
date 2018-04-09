d567475caecb324ef7f29f1f3c732181117f6060
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-09 19:22:27 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,11 @@
 class Puppy
   attr_reader :name, :breed, :age
-end
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
 ##1. Build out a puppy class in `models/puppy.rb`. Puppies should have name,
 ##breed, and age attributes. You will need to be able to pass these three

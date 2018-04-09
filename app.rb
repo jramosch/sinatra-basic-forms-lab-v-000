846cf1c29ef0ba9af457c04290a9add7a2770af9
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-09 19:49:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
   
@@ -11,6 +12,7 @@ class App < Sinatra::Base
   end
   
   post '/' do
+    @puppy = 
     erb :display_puppy
   end
 end
